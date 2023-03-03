a560f1324dae7b8065c7fb25d30d999baeedd7b3
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-03-03 22:14:33 +0200

    added data context

---
+++ AnimeReview/Data/DataContext.cs
@@ -0,0 +1,40 @@
+﻿using AnimeReview.Models;
+using Microsoft.EntityFrameworkCore;
+namespace AnimeReview.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Anime> Anime { get; set; } 
+
+        public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        public DbSet<Reviewer> Reviewers { get; set; }  
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<AnimeGenre> AnimeGenres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AnimeGenre>()
+                .HasKey(ag => new { ag.AnimeId, ag.GenreId });
+            modelBuilder.Entity<AnimeGenre>()
+                .HasOne(a => a.Anime)
+                .WithMany(ag => ag.AnimeGenres)
+                .HasForeignKey(a => a.AnimeId); 
+            modelBuilder.Entity<AnimeGenre>()
+                .HasOne(g => g.Genre)
+                .WithMany(ag => ag.AnimeGenres)
+                .HasForeignKey(g => g.GenreId);
+        }
+    }
+}

302f7be50a5ce918f508171705a9a8b61aaf045d
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-03-05 19:20:09 +0200

    dependency injection

--- AnimeReview/Program.cs
+++ AnimeReview/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<DataInitializer>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IAnimeRepository, AnimeRepository>();
+builder.Services.AddScoped<IGenreRepository, GenreRepository>();
+builder.Services.AddScoped<ICountryRepository, CountryRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

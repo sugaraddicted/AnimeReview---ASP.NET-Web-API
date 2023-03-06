3806296bc9649445c0a69bcd2565771346e79384
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-03-06 15:32:57 +0200

    added reviewer repository

---
+++ AnimeReview/Repository/ReviewerRepository.cs
@@ -0,0 +1,38 @@
+﻿using AnimeReview.Data;
+using AnimeReview.Interfaces;
+using AnimeReview.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimeReview.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+
+        public ReviewerRepository(DataContext context)
+        {
+            _context = context;  
+        }
+
+        public Reviewer GetReviewerById(int id)
+        {
+            return _context.Reviewers.Where(r => r.Id == id).Include(r => r.Reviews).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.ToList();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviewerExists(int id)
+        {
+            return _context.Reviews.Any(r => r.Id == id);
+        }
+    }
+}
